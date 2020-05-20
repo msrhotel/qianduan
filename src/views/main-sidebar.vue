0ed730676e053314ac5f57f96b3671e9dc858025
fzmsyx <sophiasaww@gmail.com>
2020-05-20 14:17:50 +0900

    注释部分不用功能

--- src/views/main-sidebar.vue
+++ src/views/main-sidebar.vue
@@ -6,10 +6,10 @@
         :collapse="sidebarFold"
         :collapseTransition="false"
         class="site-sidebar__menu">
-        <el-menu-item index="home" @click="$router.push({ name: 'home' })">
+        <!-- <el-menu-item index="home" @click="$router.push({ name: 'home' })">
           <icon-svg name="shouye" class="site-sidebar__menu-icon"></icon-svg>
           <span slot="title">首页</span>
-        </el-menu-item>
+        </el-menu-item> -->
         <el-submenu index="demo">
           <template slot="title">
             <icon-svg name="shoucang" class="site-sidebar__menu-icon"></icon-svg>
